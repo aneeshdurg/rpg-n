7f8cc28d7a95d17c57f01e7d65c23dba110dca26
Aneesh Durg <durg2@illinois.edu>
2019-07-21 14:12:40 -0400

    Add text to hp display

--- src/combat.js
+++ src/combat.js
@@ -277,15 +277,16 @@ export class UIActionSelector extends ActionSelector {
 
     this.actions = document.createElement("div");
 
+    this.attack = document.createElement("button");
+    this.attack.innerText = "attack";
+    this.attack.classList.add("action-button", "nes-btn", "is-primary");
+    this.actions.appendChild(this.attack);
+
     this.run = document.createElement("button");
     this.run.innerText = "run";
     this.run.classList.add("action-button", "nes-btn", "is-error");
     this.actions.appendChild(this.run);
 
-    this.attack = document.createElement("button");
-    this.attack.innerText = "attack";
-    this.attack.classList.add("action-button", "nes-btn", "is-primary");
-    this.actions.appendChild(this.attack);
 
     this.items = document.createElement("button");
     this.items.innerText = "items";
@@ -596,20 +597,30 @@ export class RunGame extends Action {
     var style = window.getComputedStyle(character.active_sprite);
 
     var hp_obj = {};
+    hp_obj.label = document.createElement('label');
+
+    hp_obj.status = document.createElement('p');
+    hp_obj.status.style.position = "absolute";
+    hp_obj.status.style.top = "1em";
+    hp_obj.status.style.left = "1em"; // TODO also text color and stuff
+
     hp_obj.progress = document.createElement('progress');
     hp_obj.progress.classList.add("nes-progress", "is-success");
     hp_obj.progress.max = character.max_hp;
-    character.active_sprite.parentElement.appendChild(hp_obj.progress);
 
-    hp_obj.progress.style.position = "absolute";
+    hp_obj.label.appendChild(hp_obj.status);
+    hp_obj.label.appendChild(hp_obj.progress);
+    character.active_sprite.parentElement.appendChild(hp_obj.label);
+
+    hp_obj.label.style.position = "absolute";
     if (is_below) {
-      hp_obj.progress.style.bottom = this._to_num(style.bottom) + this._to_num(style.marginBottom) + 5;
+      hp_obj.label.style.bottom = this._to_num(style.bottom) + this._to_num(style.marginBottom) + 5;
     } else {
-      hp_obj.progress.style.top = this._to_num(style.top) + this._to_num(style.marginTop) - 5;
+      hp_obj.label.style.top = this._to_num(style.top) + this._to_num(style.marginTop) - 5;
     }
-    hp_obj.progress.style.left = this._to_num(style.left) + this._to_num(style.marginLeft) + this._to_num(style.width) / 4;
-    hp_obj.progress.style.width = this._to_num(style.width) / 2;
-    hp_obj.progress.style.height = 20; // ?
+    hp_obj.label.style.left = this._to_num(style.left) + this._to_num(style.marginLeft) + this._to_num(style.width) / 4;
+    hp_obj.label.style.width = this._to_num(style.width) / 2;
+    hp_obj.label.style.height = 20; // ?
 
     //hp_obj.width = hp_obj.canvas.width / 2;
     //hp_obj.height = hp_obj.canvas.height;
@@ -649,7 +660,7 @@ export class RunGame extends Action {
         }
 
         // TODO render text outside of progress
-        hp_obj.progress.innerText =
+        hp_obj.status.innerText =
           "Lvl: " + character.level + " | HP: " + Math.floor(hp_obj.progress.value) + "/" + Math.round(character.max_hp);
 
         if (hp_obj.progress.value != character.hp) {
